58b912a50968d3fc2f8a8bd0135179e4b7154cff
Alex Jones <ajonzy@gmail.com>
2022-08-16 18:55:06 -0600

    Added database tables

--- app.py
+++ app.py
@@ -15,7 +15,62 @@ ma = Marshmallow(app)
 CORS(app)
 
 # SQLAlchemy Tables
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False, unique=False)
+    email = db.Column(db.String, nullable=False, unique=False)
+    meals = db.relationship("Meal", backref="user", cascade='all, delete, delete-orphan')
+    
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
 
+class Meal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=False)
+    description = db.Column(db.String, nullable=True, unique=False)
+    image_url = db.Column(db.String, nullable=True, unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    recipe = db.relationship("Recipe", backref="meal", cascade='all, delete, delete-orphan')
+    
+    def __init__(self, name, description, image_url, user_id):
+        self.name = name
+        self.description = description
+        self.image_url = image_url
+        self.user_id = user_id
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    meal_id = db.Column(db.Integer, db.ForeignKey("meal.id"), nullable=False)
+    steps = db.relationship("Step", backref="recipe", cascade='all, delete, delete-orphan')
+    ingredients = db.relationship("Ingredient", backref="recipe", cascade='all, delete, delete-orphan')
+    
+    def __init__(self, meal_id):
+        self.meal_id = meal_id
+
+class Step(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, nullable=False, unique=False)
+    text = db.Column(db.String, nullable=False, unique=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipe.id"), nullable=False)
+    
+    def __init__(self, number, text, recipe_id):
+        self.number = number
+        self.text = text
+        self.recipe_id = recipe_id
+
+class Ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=False)
+    amount = db.Column(db.String, nullable=False, unique=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipe.id"), nullable=False)
+    
+    def __init__(self, name, amount, recipe_id):
+        self.name = name
+        self.amount = amount
+        self.recipe_id = recipe_id
 
 # Marshmallow Schemas
 
